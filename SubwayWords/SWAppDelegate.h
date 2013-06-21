ab31a81edfa445d776baf3eb5f39681f599d0683
Fang Nan <nanfang05@gmail.com>
2013-06-22 07:58:53 +0800

    Initial Commit

---
+++ SubwayWords/SWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SWAppDelegate.h
+//  SubwayWords
+//
+//  Created by nanfang on 6/22/13.
+//  Copyright (c) 2013 LVTUXIONGDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SubwayWords/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SubwayWords
+//
+//  Created by nanfang on 6/22/13.
+//  Copyright (c) 2013 LVTUXIONGDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SWAppDelegate class]));
+    }
+}
